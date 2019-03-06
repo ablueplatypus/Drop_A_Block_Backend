f01aba327aee4b3168f08ad0b45bfcc473aefad2
Matt Doering <ablueplatypus21@gmail.com>
2019-03-06 16:33:35 -0500

    added a method to serialize to allow even zeros display on scores

--- app/serializers/stat_serializer.rb
+++ app/serializers/stat_serializer.rb
@@ -1,8 +1,11 @@
 class StatSerializer < ActiveModel::Serializer
-  attributes :high_score, :line_clear, :initials
+  attributes :high_score, :line_clear, :initials, :mod_high_score
   # def username
   #   # byebug
   #   object.user.username
   # end
 
+  def mod_high_score
+    ("00000000" + object.high_score.to_s)[-6..-1]
+  end
 end
