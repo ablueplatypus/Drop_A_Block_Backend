2da9eb78ae97a01d6e3958b778baae235fade723
Matt Doering <ablueplatypus21@gmail.com>
2019-03-15 11:11:34 -0400

    added  '/' route to stats

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
         resources :users, :stats, only: [:index, :create]
         # post 'user/login', to: 'users#login', as: 'loggin_page'
+        get '/', to: 'stats#index', as: 'heroku_home'
       end
     end
 
